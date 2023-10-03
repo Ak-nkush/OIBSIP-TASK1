308046005566c71f3d6f6c44003c8867967b63c2
Ankush <01ankush.mahadole@gmail.com>
2023-10-03 21:06:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let string = ""
+let buttons = document.querySelectorAll("button")
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", (a) => {
+        try {
+            if (a.target.innerText == "=") {
+                string = eval(string)
+                document.querySelector("input").value = string
+            }
+            else if (a.target.innerText == "AC") {
+                string = ""
+                document.querySelector("input").value = string
+            }
+            else if (a.target.innerText == "DEL") {
+                string = string.slice(0, -1)
+                document.querySelector("input").value = string
+            }
+            else {
+                string = string + a.target.innerText;
+                document.querySelector("input").value = string
+            }
+        } 
+         catch (error) {
+            // Handle the error and display it in the input field
+            string = "Error";
+            document.querySelector("input").value = string;
+            
+        }
+    });
+});
